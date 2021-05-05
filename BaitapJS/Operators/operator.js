02cf7ab267734d00994b8b3e0f6c5ca18df1ab6d
Phuoc Nguyen <nphuoc0402@gmail.com>
2021-05-05 15:57:24 +0700

    add file operator.html and operator.js

---
+++ BaitapJS/Operators/operator.js
@@ -0,0 +1,8 @@
+let inputWidth ;
+let inputHeight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+let width = parseInt (inputWidth);
+let height = parseInt (inputHeight);
+let are = width*height;
+document.write("the are is: " + are);
