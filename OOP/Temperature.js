50e3734431da8d7f5bcd5393d3ea144e73a42e6e
Phuoc Nguyen <nphuoc0402@gmail.com>
2021-05-26 16:15:56 +0700

    change file Temperature

--- OOP/Temperature.js
+++ OOP/Temperature.js
@@ -1,15 +1,18 @@
 class Temperature {
-    constructor(C){
-        this.C = C;
+  constructor(C) {
+    if (C >= -273) {
+      this.C = C;
+    }else {
+        throw `invalid Value ${C}`;
     }
-    ConvertCtoF(){
-        return this.C*(9/5)+32;
-    }
-    ConvertCtoK(){
-        return this.C + 275.15;
-    }
-
+  }
+  ConvertCtoF() {
+    return this.C * (9 / 5) + 32;
+  }
+  ConvertCtoK() {
+    return this.C + 275.15;
+  }
 }
-let temp = new Temperature(25);
+let temp = new Temperature(-290);
 console.log(temp.ConvertCtoF());
 console.log(temp.ConvertCtoK());
